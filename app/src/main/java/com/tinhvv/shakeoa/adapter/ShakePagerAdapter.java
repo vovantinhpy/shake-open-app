f82e7ab4e313c75aa32b36bbcefde4f96c5f53b9
vovantinhpy <vovantinhpy@gmail.com>
2016-02-23 00:33:52 +0700

    add new fragment

--- app/src/main/java/com/tinhvv/shakeoa/adapter/ShakePagerAdapter.java
+++ app/src/main/java/com/tinhvv/shakeoa/adapter/ShakePagerAdapter.java
@@ -2,33 +2,47 @@ package com.tinhvv.shakeoa.adapter;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
 
-import java.util.List;
+import com.tinhvv.shakeoa.fragment.DemoFragment;
+import com.tinhvv.shakeoa.fragment.ShakeOAFragment;
 
 /**
  * Created by ray-jason on 22/02/2016.
  */
-public class ShakePagerAdapter extends FragmentPagerAdapter {
+public class ShakePagerAdapter extends FragmentStatePagerAdapter {
 
-    private List<Fragment> fragments;
+    private int mNumOfTabs;
 
     public ShakePagerAdapter(FragmentManager fm) {
         super(fm);
     }
 
-    public ShakePagerAdapter(FragmentManager fm, List<Fragment> fragments) {
+    public ShakePagerAdapter(FragmentManager fm, int NumOfTabs) {
         super(fm);
-        this.fragments = fragments;
+        this.mNumOfTabs = NumOfTabs;
     }
 
     @Override
     public Fragment getItem(int position) {
-        return fragments.get(position);
+
+        switch (position) {
+            case 0:
+                DemoFragment tab1 = new DemoFragment();
+                return tab1;
+            case 1:
+                ShakeOAFragment tab2 = new ShakeOAFragment();
+                return tab2;
+            case 2:
+                ShakeOAFragment tab3 = new ShakeOAFragment();
+                return tab3;
+            default:
+                return null;
+        }
     }
 
     @Override
     public int getCount() {
-        return fragments.size();
+        return mNumOfTabs;
     }
 }
--- app/src/main/java/com/tinhvv/shakeoa/fragment/DemoFragment.java
+++ app/src/main/java/com/tinhvv/shakeoa/fragment/DemoFragment.java
@@ -1,20 +1,41 @@
 package com.tinhvv.shakeoa.fragment;
 
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.tinhvv.shakeoa.R;
 
 /**
  * Created by ray-jason on 22/02/2016.
  */
-public class DemoFragment extends Fragment {
+public class DemoFragment extends Fragment implements SensorEventListener {
 
     public static final String EXTRA_MESSAGE = "EXTRA_MESSAGE";
+
+    private float lastX, lastY, lastZ;
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+
+    private float deltaX = 0;
+    private float deltaY = 0;
+    private float deltaZ = 0;
+
+    private float vibrateThreshold = 0;
+
+    private TextView currentX, currentY, currentZ;
+
+    public Vibrator v;
 
     public static final DemoFragment newInstance(String message)
     {
@@ -26,10 +47,89 @@ public class DemoFragment extends Fragment {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        sensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
+            // success! we have an accelerometer
+
+            accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+            vibrateThreshold = accelerometer.getMaximumRange() / 2;
+        } else {
+            // fai! we dont have an accelerometer!
+        }
+
+        //initialize vibration
+        v = (Vibrator) getActivity().getSystemService(Context.VIBRATOR_SERVICE);
+    }
+
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_setting_shake, container, false);
-
+        View v = inflater.inflate(R.layout.fragment_demo, container, false);
+        currentX = (TextView) v.findViewById(R.id.currentX);
+        currentY = (TextView) v.findViewById(R.id.currentY);
+        currentZ = (TextView) v.findViewById(R.id.currentZ);
         return v;
+    }
+
+    //onResume() register the accelerometer for listening the events
+    @Override
+    public void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    //onPause() unregister the accelerometer for stop listening the events
+    @Override
+    public void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        // clean current values
+        displayCleanValues();
+        // display the current x,y,z accelerometer values
+        displayCurrentValues();
+
+        // get the change of the x,y,z values of the accelerometer
+        deltaX = Math.abs(lastX - event.values[0]);
+        deltaY = Math.abs(lastY - event.values[1]);
+        deltaZ = Math.abs(lastZ - event.values[2]);
+
+        // if the change is below 2, it is just plain noise
+        if (deltaX < 2)
+            deltaX = 0;
+        if (deltaY < 2)
+            deltaY = 0;
+        if ((deltaZ > vibrateThreshold)
+                || (deltaY > vibrateThreshold)
+                || (deltaZ > vibrateThreshold)) {
+            v.vibrate(50);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    public void displayCleanValues() {
+        currentX.setText("0.0");
+        currentY.setText("0.0");
+        currentZ.setText("0.0");
+    }
+
+    // display the current x,y,z accelerometer values
+    public void displayCurrentValues() {
+        currentX.setText(Float.toString(deltaX));
+        currentY.setText(Float.toString(deltaY));
+        currentZ.setText(Float.toString(deltaZ));
     }
 }
